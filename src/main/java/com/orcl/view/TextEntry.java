6f662d326a3eae4e7cf3900a15a3578f62fc24a8
catcat@example.com <ag9999@ua.fm>
2012-04-12 16:00:03 +0300

    1

--- src/main/java/com/orcl/view/TextEntry.java
+++ src/main/java/com/orcl/view/TextEntry.java
@@ -1,12 +1,5 @@
 package com.orcl.view;
 
-/**
- * Created by IntelliJ IDEA.
- * User: agaidai
- * Date: 2/1/12
- * Time: 2:52 PM
- * To change this template use File | Settings | File Templates.
- */
 public class TextEntry {
     protected long id;
     protected String name;
